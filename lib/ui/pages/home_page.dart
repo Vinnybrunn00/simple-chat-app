1e6f3c50ed9340b6aa1306a745499c098181bb48
vinnybrunn00 <vinibruno.dev@gmail.com>
2026-01-03 23:27:57 -0300

    add await in animateTo function

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -24,7 +24,7 @@ class _HomePageState extends State<HomePage> {
 
   void _scrollToBottom() {
     if (_scrollController.hasClients) {
-      _scrollController.animateTo(
+      await _scrollController.animateTo(
         _scrollController.position.maxScrollExtent,
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeOut,
